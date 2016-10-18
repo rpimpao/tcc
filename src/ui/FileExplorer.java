367fd3585b76b7ab016dc59941c7457cab9fb4c9
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2016-10-18 21:19:07 -0200

    Refactored some files.

---
+++ src/ui/FileExplorer.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileExplorer {
+	public FileExplorer()
+	{
+		
+	}
+	
+	public String exploreArffFiles()
+	{	
+		String filePath = "";
+		JFileChooser fileExplorer = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("ARFF Files", "arff");
+		fileExplorer.setFileFilter(filter);
+		fileExplorer.showOpenDialog(fileExplorer);
+		
+		try{
+		File file = fileExplorer.getSelectedFile();
+		filePath = file.getAbsolutePath();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return filePath;
+	}
+	
+	public String searchDir(){
+		String path = null;
+		JFileChooser fileExplorer = new JFileChooser();
+		fileExplorer.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    fileExplorer.setAcceptAllFileFilterUsed(false);
+	    fileExplorer.showOpenDialog(null);
+	    path = fileExplorer.getSelectedFile().getAbsolutePath();
+	    
+		return path;
+	}
+}
---
+++ src/wekaCore/ModelGenerator.java
@@ -0,0 +1,95 @@
+package wekaCore;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import ui.FileExplorer;
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class ModelGenerator {
+	private ArrayList<Classifier> m_models;
+	
+	public ModelGenerator(String filePath) {
+		J48 j48Model = new J48();
+		NaiveBayes naiveBayesModel = new NaiveBayes();
+		MultilayerPerceptron mlpModel = new MultilayerPerceptron();
+
+		try {
+			DataSource baseInstances = new DataSource(filePath);
+			Instances instances = baseInstances.getDataSet();
+			// TODO: set class attibute?
+			instances.setClassIndex(instances.numAttributes() - 1);
+
+			j48Model.buildClassifier(instances);
+			naiveBayesModel.buildClassifier(instances);
+			mlpModel.buildClassifier(instances);
+
+			m_models = new ArrayList<Classifier>();
+			
+			m_models.add(j48Model);
+			m_models.add(naiveBayesModel);
+			m_models.add(mlpModel);
+			
+			evaluateModels(instances);
+			
+			/// TODO: Ask to save models
+			saveModels();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void saveModels()
+	{
+		FileExplorer arf = new FileExplorer();
+		String path = arf.searchDir();
+		
+		for (int i = 0; i < m_models.size(); i++) {
+			ObjectOutputStream mlpOutputStream;
+			try {
+				mlpOutputStream = new ObjectOutputStream(
+						new FileOutputStream(path + "/" + m_models.get(i).getClass().getSimpleName() + ".model"));
+				JOptionPane.showMessageDialog(null, m_models.get(i).getClass().getSimpleName());
+				mlpOutputStream.writeObject(m_models.get(i));
+				mlpOutputStream.flush();
+				mlpOutputStream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void evaluateModels(Instances instances)
+	{
+		Evaluation eval;
+		for (int i = 0; i < m_models.size(); i++) {
+			try {
+				eval = new Evaluation(instances);
+				eval.crossValidateModel(m_models.get(i), instances, 10, new Random(1));
+				JOptionPane.showMessageDialog(null, eval.correct());
+				/// TODO: How to show the user the algorithm accuracy?
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public ArrayList<Classifier> getModels()
+	{
+		return m_models;
+	}
+}
